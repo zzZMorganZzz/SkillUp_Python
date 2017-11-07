60273617eed7e9b727e5205aafcb4f96c12c03dc
zzZMorganZzz <mikhaylov-kg@mail.ru>
2017-11-08 08:49:09 +1000

    Проверка на правильность скобок

---
+++ CheckPoint.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+
+
+"""Дан файл с каким-то математическим выражением, которое содержит скобки разных типов “{[()]}” в любом порядке.
+   Проверить сбалансированны ли скобки.
+   В случае сбалансированности вывести результат вычисления выражения, иначе указать позицию скобки,
+   которая нарушает баланс."""
+
+
+def isBracked(liter):
+    return liter in ['{', '}', '[', ']', '{', '}']
+
+
+_file = open("Task.txt")
+_task = _file.readline().replace(' ', '')
+
+
+
+
+def getReversBracked(bracked):
+    resultValue = ''
+    if bracked == '{':
+        resultValue = '}'
+    if bracked == '[':
+        resultValue = ']'
+    if bracked == '(':
+        resultValue = ')'
+    return resultValue
+
+
+def checkBracked(example):
+    isResult = True
+    openSymbolCollection = []
+    closeSymbol = None
+    for item in _task:
+        if item in ['{', '[', '(']:
+            openSymbolCollection.append(item)
+            closeSymbol = getReversBracked(item)
+        if item in ['}', ']', ')']:
+            if closeSymbol == item:
+                openSymbolCollection.pop()
+                if len(openSymbolCollection) != 0:
+                    closeSymbol = getReversBracked(openSymbolCollection[len(openSymbolCollection) - 1])
+            else:
+                isResult = False
+                break
+    return isResult
+
+
+print (checkBracked(_task))
+
+_file.close()
