0db98a5d8455db9ea3c77762bc1d36496d1b280e
zzZMorganZzz <morgan_2112@mail.ru>
2018-01-16 11:56:39 +1000

    Классовая модель псевдо твиттера

---
+++ Lesson_7_DjangoModels/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class Lesson7DjangomodelsConfig(AppConfig):
+    name = 'Lesson_7_DjangoModels'
